712cf2aa017116525ad0cc38e2ba03dd3a958113
Aliaksandr Tsiareshchanka <coffee.injoy@gmail.com>
2021-04-21 21:03:57 +0300

    push

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        const int maxValue = 999;
+        static void Main(string[] args)
+        {
+            int la = sumProgressia(3) + sumProgressia(5) - sumProgressia(15);
+            Console.WriteLine($"{la}");
+        }
+
+        static int sumProgressia(int step)
+        {
+            return step * (maxValue / step) * ((maxValue / step) + 1) / 2;
+        }
+    }
+}
