26ce0fc2a41ac826a543d48f3f148b6d3f028201
Aliaksandr Tsiareshchanka <coffee.injoy@gmail.com>
2021-04-21 22:13:26 +0300

    push

---
+++ Task5/Task5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2500; i < 999999999999999L; i++)
+            {
+                if (check(i))
+                {
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+        }
+        public static bool check(int number)
+        {
+            var posibleDividers = new int[] { 2, 3, 5, 7, 11, 13, 17, 19 };
+            foreach (var posibleD in posibleDividers)
+            {
+                if (number % posibleD != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
