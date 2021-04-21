6e942f957811ef79ac78b46ae24ea79f8ae5ae69
Aliaksandr Tsiareshchanka <coffee.injoy@gmail.com>
2021-04-21 23:36:08 +0300

    push

---
+++ Task16/Task16/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace Task16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+
+            BigInteger number = BigInteger.Pow(2, 1000);
+
+            while (number > 0)
+            {
+                result += (int)(number % 10);
+                number /= 10;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
